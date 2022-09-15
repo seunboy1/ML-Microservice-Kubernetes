1e96378cff858e5290b48303b4a457a1e351f10e
Oluwaseun Adeyo <oadeyo@seamfix.com>
2022-09-15 18:54:53 +0100

    initial commit

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 from flask.logging import create_logger
 import logging
-import sys
 # import joblib
 import pandas as pd
 from sklearn.externals import joblib
